d7b5a688f59da21abda14d681857952566439255
Chris <chrisgerth010592@gmail.com>
2021-05-21 16:37:20 -0500

    Revising trajectory generation. Still need it to work more like a true holonomic drive

--- src/main/java/frc/robot/Autonomous/Modes/Events/AutoEventDriveFig8.java
+++ src/main/java/frc/robot/Autonomous/Modes/Events/AutoEventDriveFig8.java
@@ -1,6 +1,6 @@
 package frc.robot.Autonomous.Modes.Events;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.constraint.SwerveDriveKinematicsConstraint;
 import frc.Constants;
 import frc.lib.AutoSequencer.AutoEvent;
 import frc.robot.Drivetrain.DrivetrainControl;
@@ -22,17 +23,31 @@ public class AutoEventDriveFig8 extends AutoEvent {
 	Trajectory traj;
 	
 	public AutoEventDriveFig8() {
-		config = new TrajectoryConfig(Constants.MAX_FWD_REV_SPEED_MPS, Constants.MAX_TRANSLATE_ACCEL_MPS2);
+
+
+		//////////////////////////////////////////////////////
+		// Trajectory Configuration
+		Pose2d startPose = new Pose2d(new Translation2d(3.0, 3.0), Rotation2d.fromDegrees(0));
+		Pose2d endPose = new Pose2d(new Translation2d(3.0, 3.0), Rotation2d.fromDegrees(-180));
+	
+		ArrayList<Translation2d> interiorWaypoints = new ArrayList<Translation2d>();
+		interiorWaypoints.add(new Translation2d(5.0,1.0));
+		interiorWaypoints.add(new Translation2d(5.0,1.0));
+		interiorWaypoints.add(new Translation2d(7.0,5.0));
+		interiorWaypoints.add(new Translation2d(9.0,3.0));
+		interiorWaypoints.add(new Translation2d(7.0,1.0));	
+		interiorWaypoints.add(new Translation2d(5.0,5.0));	
+		
+		double desTrajMaxSpeed = Constants.MAX_FWD_REV_SPEED_MPS/2;
+		double desTrajMaxAccel = Constants.MAX_TRANSLATE_ACCEL_MPS2/2;
+		// End Config
+		//////////////////////////////////////////////////////
+
+
+		config = new TrajectoryConfig(desTrajMaxSpeed, desTrajMaxAccel);
 		config.setKinematics(Constants.m_kinematics);
-		traj = TrajectoryGenerator.generateTrajectory( List.of(
-			new Pose2d(new Translation2d(3.0, 3.0), Rotation2d.fromDegrees(0)),
-			new Pose2d(new Translation2d(5.0, 1.0 ), Rotation2d.fromDegrees(0)),
-			new Pose2d(new Translation2d(7.0, 5.0), Rotation2d.fromDegrees(0)),
-			new Pose2d(new Translation2d(9.0, 3.0), Rotation2d.fromDegrees(-90)),
-			new Pose2d(new Translation2d(7.0, 1.0 ), Rotation2d.fromDegrees(-180)),
-			new Pose2d(new Translation2d(5.0, 5.0), Rotation2d.fromDegrees(-180)),
-			new Pose2d(new Translation2d(3.0, 3.0), Rotation2d.fromDegrees(-180))
-		), config);
+		config.addConstraint(new SwerveDriveKinematicsConstraint(Constants.m_kinematics, desTrajMaxSpeed));
+		traj = TrajectoryGenerator.generateTrajectory( startPose, interiorWaypoints, endPose, config);
 	}
 
 	@Override
